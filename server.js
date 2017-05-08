fd5a87b3fc71d001555d96cf78c655a54a670ec6
htwright <harrisontwright@gmail.com>
2017-05-08 17:50:01 -0500

    formatting

--- server.js
+++ server.js
@@ -13,46 +13,46 @@ app.use(bodyParser.json());
 
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/public/index.html'));
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 
 app.get('/items', (req, res) =>{
-  Item
+    Item
     .find()
     .then(items =>{
-      let mapped = [];
-      for(let i = 0; i < items.length; i++){
-        mapped[i] = items[i].apiRepr();
-      }
-      let mapped1 = JSON.stringify(mapped, null, 2);
-      return res.send(mapped1);
+        let mapped = [];
+        for(let i = 0; i < items.length; i++){
+            mapped[i] = items[i].apiRepr();
+        }
+        let mapped1 = JSON.stringify(mapped, null, 2);
+        return res.send(mapped1);
     });
 });
 
 
 app.post('/items', (req, res) =>{
-  const requiredFields = ['title', 'content', 'subject'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`;
-      console.error(message);
-      throw new Error(message);
+    const requiredFields = ['title', 'content', 'subject'];
+    for (let i=0; i<requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing \`${field}\` in request body`;
+            console.error(message);
+            throw new Error(message);
         // return res.status(400).send(message);
+        }
     }
-  }
 
-  Item
+    Item
     .create({
-      author: req.body.author,
-      subject: req.body.subject,
-      title: req.body.title,
-      content: req.body.content,
-      credentials: req.body.credentials
+        author: req.body.author,
+        subject: req.body.subject,
+        title: req.body.title,
+        content: req.body.content,
+        credentials: req.body.credentials
     })
     .then(() => {
-      res.status(201).send('Item sucessfully created!');
+        res.status(201).send('Item sucessfully created!');
     })
     //provide update links
     //provide feedback on input
@@ -60,85 +60,85 @@ app.post('/items', (req, res) =>{
 });
 
 app.put('/items/:id', (req,res) => {
-  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
-    res.status(400).json({
-      error: 'Request path id and request body id values must match'
-    });
-  }
-
-  const updatedItem = {};
-  const updateableFields = ['subject', 'title','content'];
-  updateableFields.forEach(field => {
-    if(field in req.body) {
-      updatedItem[field] = req.body[field];
+    if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+        res.status(400).json({
+            error: 'Request path id and request body id values must match'
+        });
     }
-  });
 
-  Item
+    const updatedItem = {};
+    const updateableFields = ['subject', 'title','content'];
+    updateableFields.forEach(field => {
+        if(field in req.body) {
+            updatedItem[field] = req.body[field];
+        }
+    });
+
+    Item
   .findByIdAndUpdate(req.params.id, {$set: updatedItem}, {new: true})
   .then(result => {
-    res.status(201).json(result.apiRepr());
+      res.status(201).json(result.apiRepr());
   })
   .catch(() => {
-    res.status(500).json({message: 'Something went wrong!!!'});
+      res.status(500).json({message: 'Something went wrong!!!'});
   });
 });
 
 app.delete('/items/:id', (req,res) => {
-  Item
+    Item
     .findByIdAndRemove(req.params.id)
     .then(() => {
-      res.status(204).json({message: 'Item successfully deleted!!!'});
+        res.status(204).json({message: 'Item successfully deleted!!!'});
     })
     .catch(err => {
-      console.log(err);
-      res.status(500).json({error: 'Something went wrong!!!'});
+        console.log(err);
+        res.status(500).json({error: 'Something went wrong!!!'});
     });
 });
 //thinkful
 //post.ly
 app.use('*', function(req, res) {
-  res.status(404).json({message: 'Not Found'});
+    res.status(404).json({message: 'Not Found'});
 });
 
 let server;
 
 function runServer(databaseUrl=DATABASE_URL, port=PORT) {
-  return new Promise((resolve, reject) => {
-    mongoose.connect(databaseUrl, err => {
-      if (err) {
-        return reject(err);
-      }
-      server = app.listen(port, () => {
-        console.log(`Your app is listening on port ${port}`);
-        resolve();
-      })
+    return new Promise((resolve, reject) => {
+        mongoose.connect(databaseUrl, err => {
+            if (err) {
+                return reject(err);
+            }
+            server = app.listen(port, () => {
+                console.log(`Your app is listening on port ${port}`);
+                resolve();
+            })
       .on('error', err => {
-        mongoose.disconnect();
-        reject(err);
+          mongoose.disconnect();
+          reject(err);
       });
+        });
     });
-  });
 }
 
 function closeServer() {
-  return mongoose.disconnect().then(() => {
-    return new Promise((resolve, reject) => {
-      console.log('Closing server');
-      server.close(err => {
-        if (err) {
-          return reject(err);
-        }
-        resolve();
-      });
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing server');
+            server.close(err => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
     });
-  });
 }
 
 // if server.js is called directly (aka, with `node server.js`), this block
 // runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
 if (require.main === module) {
-  runServer().catch(err => console.error(err));
+    runServer().catch(err => console.error(err));
 }
 
 module.exports = {runServer, app, closeServer};
