20fe25d57ac0beb0726c1eff8a1e23816f32b517
htwright <harrisontwright@gmail.com>
2017-05-08 10:58:39 -0500

    first test working

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
+
 app.listen(8080 || process.env.PORT, () =>{
     console.log('App listening on 8080');
 });
+
+module.exports = {app};
