e0153f8c7223acac64eb30478350ef8a2f2caffb
htwright <harrisontwright@gmail.com>
2017-05-11 21:36:00 -0500

    show all button

--- public/js/client.js
+++ public/js/client.js
@@ -1,79 +1,7 @@
 let URL = 'https://safe-earth-98661.herokuapp.com/items';
 // let URL = 'http://localhost:8080/items';
 
-function renderSelector(opt, term = ''){
-  if (opt === 'full'){
-    return getAll()
-    .then(res =>{
-      render(res);
-      return;
-    })
-    .then(hideReadAlls())
-    .catch(err => console.error(err));
-  } else if (opt === 'search'){
-    return new Promise((resolve, reject)=>{
-      return getBySearchTerm(term)
-    .then(res => {
-      render(res);
-      return;
-    })
-    .then(hideReadAlls())
-    .catch(err => console.error(err)); 
-    });
-  }
-}
-function render(arr){
-  let html = '';
-  let shortContent;
-  // let toRender = [];
-  console.log(arr);
-  arr.forEach(function(item){
-    shortContent = '';
-    if(item.content.length > 250){
-      shortContent = item.content.substring(0, 250)+'...';
-    }
-    html += 
-`<li class = 'item' id = '${item.id}'>
-<div class = 'main-container'>
-  <div class='header-container'>
-    <p class = 'title'>${item.title}</p>
-    <h3 class = 'subject'>${item.subject}</h3>
-  </div>
-  <div class='content-container'>
-    <p class = 'author'>Author: ${item.author}</p>
-    <p class = 'credentials'>Credentials: ${item.credentials}</p>
-    <p class = 'truncated-content'>${shortContent}</p>
-    <p class = 'full-content hidden'>${item.content}</p>
-  </div>
-  <div class ='controls-container'>
-    <button class = 'read-more'>Read More</button>
-    <button class = 'read-more hidden'>Read Less</button>
-    <button class = "edit-button" type="button"> Edit </button>
-  </div>
-</div>
-  
-<form class = 'edit-form hidden'>
-  <label>
-    Title<br> <input class='edit-title' type="text" name = 'content' value = '${item.title}'><br>
-  </label>
-  <label>
-    Subject<br> <input class='edit-subject' type="text" name = 'content' value = '${item.subject}'><br>
-    </label>
-    <label>
-    Credentials<br> <input class='edit-credentials' type="text" name = 'content' value = '${item.credentials}'><br>
-    </label>
-    <label>
-    Content<br> <textarea rows='10' cols='80' class='edit-content' type="text" name = 'content'>${item.content}</textarea><br>
-    </label>
-    <input class='edit-submit' type="submit" value="Submit">
-    <button class = "delete-submit" type="button"> Delete </button>
-  <button class = "edit-cancel" type="button"> Cancel </button>
 
-</form>
-</li>`;
-    $('.containerJS').html(html);
-  });
-}
 
 function getAll(){
   return fetch(URL)
@@ -129,6 +57,7 @@ function updateItemById(id, thisBody){
   })
   .catch(err => console.error(err));
 }
+
 function getBySearchTerm(term){
   let resultArr = [];
   let splitArr = [];
@@ -138,25 +67,92 @@ function getBySearchTerm(term){
     console.log(res);
     res.forEach(function(item){
       splitArr = item.content.split(' ');
-      //split arr = array of words from item content
-      //item = object from result array
-      //item1 = word from item content array
-      //
       splitArr.forEach(function(item1) {
         if(term.includes(item1)){
           return resultArr.push(item);
         }
       });
     });
-    //   if(splitArr.includes(term))
-    //     resultArr.push(item);
-    // });
     console.log(resultArr);
     return resultArr;
   })
   .catch(err => console.error(err));
 }
 
+function renderSelector(opt, term = ''){
+  if (opt === 'full'){
+    return getAll()
+    .then(res =>{
+      render(res);
+      return;
+    })
+    .then(hideReadAlls())
+    .catch(err => console.error(err));
+  } else if (opt === 'search'){
+    return new Promise((resolve, reject)=>{
+      return getBySearchTerm(term)
+    .then(res => {
+      render(res);
+      return;
+    })
+    .then(hideReadAlls())
+    .catch(err => console.error(err)); 
+    });
+  }
+}
+
+function render(arr){
+  let html = '';
+  let shortContent;
+  // let toRender = [];
+  console.log(arr);
+  arr.forEach(function(item){
+    shortContent = '';
+    if(item.content.length > 250){
+      shortContent = item.content.substring(0, 250)+'...';
+    }
+    html += 
+`<li class = 'item' id = '${item.id}'>
+<div class = 'main-container'>
+  <div class='header-container'>
+    <p class = 'title'>${item.title}</p>
+    <h3 class = 'subject'>${item.subject}</h3>
+  </div>
+  <div class='content-container'>
+    <p class = 'author'>Author: ${item.author}</p>
+    <p class = 'credentials'>Credentials: ${item.credentials}</p>
+    <p class = 'truncated-content'>${shortContent}</p>
+    <p class = 'full-content hidden'>${item.content}</p>
+  </div>
+  <div class ='controls-container'>
+    <button class = 'read-more'>Read More</button>
+    <button class = 'read-more hidden'>Read Less</button>
+    <button class = "edit-button" type="button"> Edit </button>
+  </div>
+</div>
+  
+<form class = 'edit-form hidden'>
+  <label>
+    Title<br> <input class='edit-title' type="text" name = 'content' value = '${item.title}'><br>
+  </label>
+  <label>
+    Subject<br> <input class='edit-subject' type="text" name = 'content' value = '${item.subject}'><br>
+    </label>
+    <label>
+    Credentials<br> <input class='edit-credentials' type="text" name = 'content' value = '${item.credentials}'><br>
+    </label>
+    <label>
+    Content<br> <textarea rows='10' cols='80' class='edit-content' type="text" name = 'content'>${item.content}</textarea><br>
+    </label>
+    <input class='edit-submit' type="submit" value="Submit">
+    <button class = "delete-submit" type="button"> Delete </button>
+  <button class = "edit-cancel" type="button"> Cancel </button>
+
+</form>
+</li>`;
+    $('.containerJS').html(html);
+  });
+}
 $(document).ready(function(){
   
   renderSelector('full');
@@ -188,6 +184,10 @@ $(document).ready(function(){
       .catch(err => {
         console.error(err);
       }); 
+  });
+
+  $('#show-all-button').on('click', function(){
+    renderSelector('full');
   });
 
   $('.containerJS').on('click', '.edit-button', function(){
